964323e6d9cb3f6e37b689d50da1c71d18e18dfd
Carlos Eduardo <carloseduardopaschoal@hotmail.com>
2024-07-18 18:26:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DIO Design Patterns com Java
---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setCompotamento(normal);
+        robo.mover();
+        robo.setCompotamento(defensivo);
+        robo.mover();
+        robo.mover();
+        robo.setCompotamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
+}
